794d97b2ee121a1b2ecde156aa3873f6695e9e73
Stefan von Buddenbrock <stefan@von-buddenbrock.com>
2023-06-19 18:19:45 +0200

    Removed Django bloat

--- sunny_jim_webapp/live_monitoring/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import live_page
-
-urlpatterns = [
-    path('', live_page, name='live-page')
-]
--- sunny_jim_webapp/live_monitoring/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def live_page(request):
-    return render(request, 'live_monitoring/live_page.html')
