aa5c4c0efe6808a033f9b6cd743764f5d8ec6e57
Stefan von Buddenbrock <stefan@von-buddenbrock.com>
2023-06-30 21:56:37 +0200

    Made the daemon into a runnable coroutine.

--- run_daemon.py
+++ run_daemon.py
@@ -2,27 +2,17 @@ import argparse
 import configuration
 from communication.connect_devices import run_devices
 from communication.attach_observers import attach_observers
-from communication.devices import CommandType
 import logging
 import asyncio
 import signal
 
 log = logging.getLogger("Daemon")
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', help='Path to config file', default='config.yaml')
-    args = parser.parse_args()
-
-    config = configuration.load_config(args.config)
-    configuration.configure_logging(config)
-
+async def daemon(config: dict):
     asynchronous_tasks = []
 
-    loop = asyncio.new_event_loop()
-    running_devices = loop.run_until_complete(run_devices(config))
-
-    inverter = running_devices["kodak_ogx_548"]
+    loop = asyncio.get_event_loop()
+    running_devices = await run_devices(config)
 
     if len(running_devices) == 0:
         log.error("No devices running!")
@@ -45,4 +35,19 @@ if __name__ == '__main__':
         for signal_type in [signal.SIGINT, signal.SIGTERM, signal.SIGHUP]:
             loop.add_signal_handler(signal_type, lambda: asyncio.ensure_future(shutdown(signal_type)))
 
-        loop.run_until_complete(asyncio.wait(asynchronous_tasks))
+        await asyncio.gather(*asynchronous_tasks)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', help='Path to config file', default='config.yaml')
+    args = parser.parse_args()
+
+    config = configuration.load_config(args.config)
+    configuration.configure_logging(config)
+
+    log.info("Starting daemon...")
+    loop = asyncio.new_event_loop()
+
+    loop.run_until_complete(daemon(config))
+    
